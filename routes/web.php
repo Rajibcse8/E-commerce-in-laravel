d72e3e1d048fb2045b3d7b1c4743e0b36a89ad3c
Rajibcse8 <rajibtalukder119@gmail.com>
2021-09-12 00:00:06 +0600

    Admin profile part 1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Backend\AdminProfileController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -38,9 +39,11 @@ Route::middleware(['auth:sanctum,admin', 'verified'])->get('admin/dashboard', fu
 
 
 
-//-----------------------------Admin--------------------------------------
+//-----------------------------Admin Route--------------------------------------
 
 Route::get('/logout',[AdminController::class,'destroy'])->name('admin.logout');
+Route::get('admin/profile',[AdminProfileController::class,'AdminProfile'])->name('admin.profile');
+
 
 
 //----------------------------------------------------------
