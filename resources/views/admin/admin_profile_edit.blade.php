b7eca0cce5b13852bcccdcbe1f6376da679e7ade
Rajibcse8 <rajibtalukder119@gmail.com>
2021-09-12 15:06:32 +0600

    ADmin profile Update Part 3 Ans Load Image jusing jquery

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -1,5 +1,6 @@
 @extends('admin.admin_master')
 @section('admin')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <div class="container-full">
 
         <!-- Main content -->
@@ -47,28 +48,20 @@
                                                 <div class="form-group">
                                                     <h5>File Input Field <span class="text-danger">*</span></h5>
                                                     <div class="controls">
-                                                        <input type="file" name="file" class="form-control" required="">
-                                                        <div class="help-block"></div>
+                                                        <input type="file" name="file" class="form-control" required="" id="image">
+                                                        
                                                     </div>
                                                 </div>
 
                                             </div>
                                         
                                             <div class="col-md-6">
-                                                <img src="{{ (!empty($editData->profile_photo_path)) ?
+                                                <img id="Showimage" src="{{ (!empty($editData->profile_photo_path)) ?
                                                     url('upload/admin_images').$editdata->profile_photo_path : url('upload/no_image.jpg')  }} "
                                                     style="width:100px; height:100px;">
 
                                             </div>
                                         </div>
-
-
-
-
-                                       
-
-
-
 
 
                                         <div class="text-xs-right">
@@ -89,6 +82,21 @@
 
         <!-- /.content -->
     </div>
+
+  <script type="text/javascript">
+     
+      $(document).ready(function () {
+          $('#image').change(function (e) { 
+            var reader= new FileReader();
+            reader.onload=function(e){
+                $('#Showimage').attr('src',e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+             
+         });
+     });
+
+  </script>
 
 
 @endsection
