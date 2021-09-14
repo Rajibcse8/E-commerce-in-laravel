347113e007ee6770e1157100bf85ddfecbb8f49c
Rajibcse8 <rajibtalukder119@gmail.com>
2021-09-14 23:42:40 +0600

    Rester user password using mailtrap.io

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,4 +1,58 @@
-<x-guest-layout>
+@extends('frontend.frontend_master')
+@section('content')
+
+
+    <div class="breadcrumb">
+        <div class="container">
+            <div class="breadcrumb-inner">
+                <ul class="list-inline list-unstyled">
+                    <li><a href="home.html">Home</a></li>
+                    <li class='active'>Forget-Password</li>
+                </ul>
+            </div><!-- /.breadcrumb-inner -->
+        </div><!-- /.container -->
+    </div><!-- /.breadcrumb -->
+
+    <div class="body-content">
+        <div class="container">
+            <div class="sign-in-page">
+                <div class="row">
+                    <!-- Sign-in -->
+                    <div class="col-md-6 col-sm-6 sign-in">
+                        <h4 class="">Forget-Password ? No Problem :)</h4>
+	                     
+                    
+                        <form method="POST" action="{{ route('password.email') }}">
+                            @csrf
+                            <div class="form-group">
+                                <label class="info-title" for="exampleInputEmail1">Enter recoverd Email Address <span>*</span></label>
+                                <input type="email" class="form-control unicase-form-control text-input" id="email"
+                                    name="email">
+                            </div>
+                            
+                            
+                            <button type="submit" class="btn-upper btn btn-primary checkout-page-button">Password Reset link </button>
+                            </form>
+                    </div>
+                  
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
+@endsection
+
+
+
+
+
+
+
+
+
+
+{{-- <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
@@ -31,4 +85,4 @@
             </div>
         </form>
     </x-jet-authentication-card>
-</x-guest-layout>
+</x-guest-layout> --}}
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,4 +1,68 @@
-<x-guest-layout>
+@extends('frontend.frontend_master')
+@section('content')
+
+
+    <div class="breadcrumb">
+        <div class="container">
+            <div class="breadcrumb-inner">
+                <ul class="list-inline list-unstyled">
+                    <li><a href="home.html">Home</a></li>
+                    <li class='active'>Reset-Password</li>
+                </ul>
+            </div><!-- /.breadcrumb-inner -->
+        </div><!-- /.container -->
+    </div><!-- /.breadcrumb -->
+
+    <div class="body-content">
+        <div class="container">
+            <div class="sign-in-page">
+                <div class="row">
+                    <!-- Sign-in -->
+                    <div class="col-md-6 col-sm-6 sign-in">
+                        <h4 class="">Rerset-Password </h4>
+	                     
+                        <form method="POST" action="{{ route('password.update') }}">
+                            @csrf
+                            <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                            <div class="form-group">
+                                <label class="info-title" for="exampleInputEmail1">Enter Email Address <span>*</span></label>
+                                <input type="email" class="form-control unicase-form-control text-input" id="email"
+                                    name="email"  required>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="info-title" for="exampleInputPassword1">Password <span>*</span></label>
+                                <input type="password" class="form-control unicase-form-control text-input" id="password" name="password" required>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="info-title" for="exampleInputPassword1">Confirm Password <span>*</span></label>
+                                <input type="password" class="form-control unicase-form-control text-input" id="password_confirmation" name="password_confirmation" required>
+                            </div>
+                            
+                            
+                            <button type="submit" class="btn-upper btn btn-primary checkout-page-button">Password Reset link </button>
+                            </form>
+                    </div>
+                  
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
+@endsection
+
+
+
+
+
+
+
+
+
+
+{{-- <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
@@ -33,4 +97,4 @@
             </div>
         </form>
     </x-jet-authentication-card>
-</x-guest-layout>
+</x-guest-layout> --}}
