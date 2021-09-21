01a7072d9cbd7a16c3e3849bdc7237bc1aef16ab
Rajibcse8 <rajibtalukder119@gmail.com>
2021-09-22 01:45:36 +0600

    Complete Dynamic DropDown list between category and sub-category for update sub-sub-category data

--- resources/views/admin/subsubcategory/subsubcategory_edit.blade.php
+++ resources/views/admin/subsubcategory/subsubcategory_edit.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.admin_master')
 @section('admin')
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <div class="container-full">
       <section class="content">
           <div class="row">
@@ -14,7 +14,7 @@
                     <!-- /.box-header -->
                     <div class="box-body">
                         <div class="table-responsive">
-                            <form method="post" action="{{ route('subsubcategory.store') }}">
+                            <form method="post" action="">
                                 @csrf
 
                                 <div class="form-group">
@@ -96,5 +96,36 @@
 
       </section>
   </div>
+
+  <script type="text/javascript">
+    
+    $(document).ready(function () {
+         
+         $('select[name="category_id"]').on('change', function () {
+            var category_id=$(this).val();
+            //alert(category_id);
+            if(category_id){
+                $.ajax({
+                    type: "GET",
+                    url: "{{ url('subcategory/find/formcategory') }}/" + category_id,
+                    dataType: "json",
+                    success: function (data) {
+                       var d=  $('select[name="subcategory_id"]').empty();
+                        $.each(data, function (key, value) { 
+                            $('select[name="subcategory_id"]').append(
+                                    '<option value="' + value.id + '">' + value
+                                    .subcategory_name_en + '</option>');
+                        });
+                    },
+                });
+            }
+            else{
+                alert('danger');
+            }
+         });
+
+    });
+
+  </script>
   
 @endsection
