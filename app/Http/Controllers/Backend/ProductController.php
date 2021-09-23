0b6071efcccde50a31c531337e4fa88b7006c573
Rajibcse8 <rajibtalukder119@gmail.com>
2021-09-24 01:27:05 +0600

    Product add page design part-1

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -5,9 +5,23 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+ use App\Models\Product;
+ use App\Models\Category;
+ use App\Models\SubCategoty;
+ use App\Models\SubSubCategory;
+ use App\Models\Brand;
+
 class ProductController extends Controller
 {
     public function AddProduct(){
+
+         $categories=Category::latest()->get();
+         $brands=Brand::latest()->get();
+       
+        
+        
+        return view('admin.product.product_add',compact('brands','categories'));
         
     }
+    
 }
