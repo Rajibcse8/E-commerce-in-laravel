485b2d3db65b0923984b9c9bb46510e23a9ed7c8
Rajibcse8 <rajibtalukder119@gmail.com>
2021-10-16 22:55:26 +0600

    Product Tags add Dynamically inn frontend theme

---
+++ resources/views/frontend/product/product_tags.blade.php
@@ -0,0 +1,36 @@
+@php
+
+$product_tag_en = App\Models\Product::groupBy('product_tag_en')
+    ->select('product_tag_en')
+    ->get();
+$product_tag_ban = App\Models\Product::groupBy('product_tag_ban')
+    ->select('product_tag_ban')
+    ->get();
+
+@endphp
+
+
+<div class="sidebar-widget product-tag wow fadeInUp">
+    <h3 class="section-title">{{ session()->get('language') == 'bangla' ? 'পণ্যের ট্যাগ' : 'Product tags' }}</h3>
+    <div class="sidebar-widget-body outer-top-xs">
+
+        <div class="tag-list">
+            @if(session()->get('language')=='bangla')
+            @foreach ($product_tag_en as $tag)
+            <a class="item" title="Phone" href="category.html">{{ str_replace(',','',$tag->product_tag_en) }}</a>
+    
+            @endforeach
+            @else
+            @foreach ($product_tag_ban as $tag)
+            <a class="item" title="Phone" href="category.html">{{ $tag->product_tag_ban }}</a>
+    
+            @endforeach
+
+            @endif
+            
+    
+        </div>
+        <!-- /.tag-list -->
+    </div>
+    <!-- /.sidebar-widget-body -->
+</div>
