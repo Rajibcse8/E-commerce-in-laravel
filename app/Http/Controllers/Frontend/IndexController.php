a4d003464d15c57596fd9198418b408c2683e5ce
Rajibcse8 <rajibtalukder119@gmail.com>
2021-10-18 23:44:04 +0600

    tag-wise product get dynamically in gird virw

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -106,8 +106,13 @@ class IndexController extends Controller
 
    public function ProductTagsView($tags){
 
-       $products=Product::where('product_tag_en',$tags)->where('product_tag_ban',$tags)->
-       OrderBY('id','DESC')->get();
+   
+
+       $products=Product::where('status',1)->where('product_tag_en',$tags)->
+       orderBY('id','DESC')->get();
+
+    //    dd(var_dump($products));
+
        $categories=Category::OrderBy('category_name_en','ASC')->get();
 
        return view('frontend.product.tagwise_product_view',compact('products','categories'));
