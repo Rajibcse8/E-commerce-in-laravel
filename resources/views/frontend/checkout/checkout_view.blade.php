ee19332816e77cab4f3f75d6a350a7fb5b3dd7b5
Rajibcse8 <rajibtalukder119@gmail.com>
2022-02-12 23:49:37 +0600

    Check out page styling

--- resources/views/frontend/checkout/checkout_view.blade.php
+++ resources/views/frontend/checkout/checkout_view.blade.php
@@ -27,13 +27,7 @@
 <div class="panel panel-default checkout-step-01">
 
 	<!-- panel-heading -->
-		<div class="panel-heading">
-    	<h4 class="unicase-checkout-title">
-	        <a data-toggle="collapse" class="" data-parent="#accordion" href="#collapseOne">
-	          <span>1</span>Checkout Method
-	        </a>
-	     </h4>
-    </div>
+	
     <!-- panel-heading -->
 
 	<div id="collapseOne" class="panel-collapse collapse in">
@@ -96,91 +90,12 @@
 
 	</div><!-- row -->
 </div>
-<!-- checkout-step-01  -->
-					    <!-- checkout-step-02  -->
-					  	<div class="panel panel-default checkout-step-02">
-						    <div class="panel-heading">
-						      <h4 class="unicase-checkout-title">
-						        <a data-toggle="collapse" class="collapsed" data-parent="#accordion" href="#collapseTwo">
-						          <span>2</span>Billing Information
-						        </a>
-						      </h4>
-						    </div>
-						    <div id="collapseTwo" class="panel-collapse collapse">
-						      <div class="panel-body">
-						      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-						      </div>
-						    </div>
-					  	</div>
-					  	<!-- checkout-step-02  -->
+<!-- checkout-step-01 End -->
+					
 
-						<!-- checkout-step-03  -->
-					  	<div class="panel panel-default checkout-step-03">
-						    <div class="panel-heading">
-						      <h4 class="unicase-checkout-title">
-						        <a data-toggle="collapse" class="collapsed" data-parent="#accordion" href="#collapseThree">
-						       		<span>3</span>Shipping Information
-						        </a>
-						      </h4>
-						    </div>
-						    <div id="collapseThree" class="panel-collapse collapse">
-						      <div class="panel-body">
-						      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-						      </div>
-						    </div>
-					  	</div>
-					  	<!-- checkout-step-03  -->
-
-						<!-- checkout-step-04  -->
-					    <div class="panel panel-default checkout-step-04">
-						    <div class="panel-heading">
-						      <h4 class="unicase-checkout-title">
-						        <a data-toggle="collapse" class="collapsed" data-parent="#accordion" href="#collapseFour">
-						        	<span>4</span>Shipping Method
-						        </a>
-						      </h4>
-						    </div>
-						    <div id="collapseFour" class="panel-collapse collapse">
-							    <div class="panel-body">
-							     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-							    </div>
-					    	</div>
-						</div>
-						<!-- checkout-step-04  -->
-
-						<!-- checkout-step-05  -->
-					  	<div class="panel panel-default checkout-step-05">
-						    <div class="panel-heading">
-						      <h4 class="unicase-checkout-title">
-						        <a data-toggle="collapse" class="collapsed" data-parent="#accordion" href="#collapseFive">
-						        	<span>5</span>Payment Information
-						        </a>
-						      </h4>
-						    </div>
-						    <div id="collapseFive" class="panel-collapse collapse">
-						      <div class="panel-body">
-						       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-						      </div>
-						    </div>
-					    </div>
-					    <!-- checkout-step-05  -->
-
-						<!-- checkout-step-06  -->
-					  	<div class="panel panel-default checkout-step-06">
-						    <div class="panel-heading">
-						      <h4 class="unicase-checkout-title">
-						        <a data-toggle="collapse" class="collapsed" data-parent="#accordion" href="#collapseSix">
-						        	<span>6</span>Order Review
-						        </a>
-						      </h4>
-						    </div>
-					    	<div id="collapseSix" class="panel-collapse collapse">
-					      		<div class="panel-body">
-					        		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-					      		</div>
-					    	</div>
-					  	</div>
-					  	<!-- checkout-step-06  -->
+					
+					  
+						
 					  	
 					</div><!-- /.checkout-steps -->
 				</div>
